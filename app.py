98c11d284a12afa93e51dd8162fbf249f99fafb4
Gavin Burke <gavburke85@gmail.com>
2022-01-25 09:48:05 +0000

    Update app.py
    Syntax error

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/test/")
-def test()
-    return "<p>Hello from /test/ page!</p>"
-
 @app.route("/")
 def hello():
     return "<p>Hello, from gavinburke.azurewebsites.net</p>"
+
+@app.route("/test")
+def test():
+    return "<p>Hello from /test page!</p>"
