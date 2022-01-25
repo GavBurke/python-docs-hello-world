2c590855deb792e8a41e3d34bed3e61dbb444218
Gavin Burke <gavburke85@gmail.com>
2022-01-25 09:59:42 +0000

    Update app.py
    Does the app.route ordering matter?

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<p>Hello, from gavinburke.azurewebsites.net</p>"
+
+@app.route('/<path:anything>')
+def show_anything(anything):
+    return f'<p>Hello from dynamic {escape(anything)} page</p>'
 
 @app.route("/test")
 def test():
     return "<p>Hello from specific /test page!</p>"
 
-@app.route('/<path:anything>')
-def show_anything(anything):
-    return f'<p>Hello from dynamic {escape(anything)} page</p>'
+@app.route("/")
+def hello():
+    return "<p>Hello, from gavinburke.azurewebsites.net</p>"
